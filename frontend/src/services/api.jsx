3f58b3220abbc69a6c64323cdf277603eb8a0f35
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-10-09 13:01:52 +0300

    Add services.yml, change drupal localhost address on frontend

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:50380/jsonapi/';
+const API_URL = "http://localhost:52142/jsonapi";
 
 export const fetchContent = async (contentType) => {
   const response = await axios.get(`${API_URL}/${contentType}`);
